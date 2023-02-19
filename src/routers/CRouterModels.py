794feb847d604b4ed7b8b7c3e28d6bec16cb248d
iseletkov <iseletkov@gmail.com>
2023-02-19 15:11:31 +0500

    Добавлены комментарии.

--- src/routers/CRouterModels.py
+++ src/routers/CRouterModels.py
@@ -3,6 +3,10 @@ from ..services.CServiceModels import my_sum
 from ..model.CMushroom import CMushroom
 from ..services.CServiceMushroomClassification import CServiceMushroomClassification
 
+# *******************************************************************************************************
+# * Роутер для обработки HTTP REST запросов к пути /models.                                             *
+# * @author Селетков И.П. 2023 0218.                                                                    *
+# *******************************************************************************************************
 router = APIRouter(
     prefix="/models",
     tags=["models"],
@@ -22,6 +26,9 @@ def m_sum(x: float = 0, y: float = 0):
     return my_sum(x, y)
 
 
+# *******************************************************************************************************
+# * Классификация гриба.                                                                                *
+# *******************************************************************************************************
 @router.post("/mushroom-classification")
 def mushroom_classification(mushroom: CMushroom):
     return serviceMushrooms.predict(mushroom)
